92228fc10b5ce214fae78cacf75571fe7d44da8a
Kailash Rajput <98249911+Kailash8799@users.noreply.github.com>
2023-09-19 21:56:20 +0530

    Update dburl.dart

--- lib/constants/dburl.dart
+++ lib/constants/dburl.dart
@@ -1,5 +1,5 @@
 const mongoUri =
-    "mongodb+srv://travelappbykailash:CuSX4iCv0lMI5ydtbyKailash@travel.tlrdkqg.mongodb.net/travelapp?retryWrites=true&w=majority";
+    "add mongo db url";
 
 const userCollection = "users";
 const bookingCollection = "booking";
